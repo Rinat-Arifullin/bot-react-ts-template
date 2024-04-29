3f78599a66524a2b98a735ac968d2f5ed607382f
arifullinrd <arifullinrd@mos-team.ru>
2024-04-29 23:25:03 +0300

    react ui kit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import { WebAppProvider } from '@vkruglikov/react-telegram-web-app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WebAppProvider>
+      <App />
+    </WebAppProvider>
   </React.StrictMode>,
 )
